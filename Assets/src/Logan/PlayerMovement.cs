c24ad96363682a98fbc24c57b36e1be2f12c02a6
Logan Finley <finley4.logan@gmail.com>
2023-02-08 12:01:13 -0800

    Created the running and idle animations for the character

--- Assets/src/Logan/PlayerMovement.cs
+++ Assets/src/Logan/PlayerMovement.cs
@@ -3,21 +3,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private float dirX = 0.0f;
+
     [SerializeField] float runSpeed = 10.0f;
     [SerializeField] float jumpSpeed = 10.0f;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float dirX = Input.GetAxis("Horizontal");
+        dirX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(dirX * runSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump")) {
             rb.velocity = new Vector2(0, jumpSpeed);
+        }
+        AnimUpdate();
+
+    }
+
+    private void AnimUpdate()
+    {
+        if (dirX > 0f) {
+            sprite.flipX = false;
+            anim.SetBool("isRunning", true);
+        }
+        else if (dirX < 0f) {
+            sprite.flipX = true;
+            anim.SetBool("isRunning", true);
+        }
+        else {
+            anim.SetBool("isRunning", false);
         }
     }
 }
