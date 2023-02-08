7bae03e77a79dfbfa7d373d075767963c9dbae80
Logan Finley <finley4.logan@gmail.com>
2023-02-08 01:32:12 -0800

    Implemented player movement

--- Assets/src/Logan/PlayerMovement.cs
+++ Assets/src/Logan/PlayerMovement.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode rightKey = KeyCode.D;
+
+    private Rigidbody2D rb;
+    private float runSpeed = 10.0f;
+    private float jumpSpeed = 10.0f;
+    private Vector2 direction;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * runSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector2(0, jumpSpeed);
+        }
     }
 }
